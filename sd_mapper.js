b29c399b94aaa26ec837ef8ee9ccb084a7ba25c6
austin inmon <Ainmon987@west-mec.org>
2019-02-12 15:17:16 -0700

    JavaScript file update final commit
    Added the variable that gives the current date and time. Added the variable that takes the time that the users computer has and put the time on the page. Created formula to decide witch image to display on the web page. Added the variable that Displays the certain sky image. Added comments. Ready for review.

--- sd_mapper.js
+++ sd_mapper.js
@@ -9,3 +9,19 @@
    Date: 2.12.19
 
 */
+//The variable gives the current date and time. 
+var thisTime = new Date();
+//The variable takes the time that the users computer has and put the time on the page. 
+var timeStr = thisTime.toLocaleString();
+//
+document.getElementById("timeStamp").innerHTML = timeStr;
+//getting the hour from thisTime variable. 
+var thisHour = thisTime.getHours();
+//getting the month from thisTime varible.
+var thisMonth = thisTime.getMonth();
+//this is the formula to decide witch image to display on the web page.
+var mapNum = (2 * thisMonth + thisHour) % 24;
+//Getting the image that belongs on the page. 
+var imgStr = "<img src='sd_sky" + mapNum + ".png' />";
+//Displaying the certain sky image. 
+document.getElementById("planisphere").insertAdjacentHTML('afterbegin', imgStr);
