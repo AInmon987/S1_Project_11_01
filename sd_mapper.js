b9b0012ecc822383cba03e9079007a11fc68a78f
austin inmon <Ainmon987@west-mec.org>
2019-02-12 15:07:47 -0700

    Inital/Change file names and added js file.
    Changed the html and js file names to proper names. Added the script link for the js file.

--- sd_mapper.js
+++ sd_mapper.js
@@ -1,11 +1,11 @@
+"use strict";
 /*
    New Perspectives on HTML5 and CSS3, 7th Edition
    Tutorial 9
    Case Problem 1
 
    Planisphere Script
-   Author: 
-   Date:   
+   Author: Austin Inmon
+   Date: 2.12.19
 
 */
-
